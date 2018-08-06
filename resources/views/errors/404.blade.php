c520c10922be2490683bc0b8d41c397d19a0f977
Desktop sa Bahay <butc@home.lang>
2018-08-06 14:17:12 +0800

    Add 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('https://vignette.wikia.nocookie.net/steamtradingcards/images/5/58/Nyan_Cat_Lost_In_Space_Artwork_1.jpg/revision/latest?cb=20151231064325')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="page-heading">
+                <h1>404 Found</h1>
+                <span class="subheading">Don't guest please refer to the links provided.</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <!-- Main Content -->
+      <div class="container">
+          <center><img src="https://crajun.com/wp-content/uploads/2014/10/morpheus_meme.jpg"></center>
+      </div>
+@endsection
