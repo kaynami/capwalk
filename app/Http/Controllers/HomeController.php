67917436833719aea38086417318f48f9cc40056
Desktop sa Bahay <butc@home.lang>
2018-08-06 08:25:57 +0800

    fix about page if no data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
-use Yajra\DataTables\Utilities\Request;
 
 class HomeController extends Controller
 {
@@ -29,7 +28,7 @@ class HomeController extends Controller
         ->orderBy('created_at', 'desc')
         ->paginate(8);
 
-        
+
         return view('home', compact('posts'));
     }
 
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,14 +15,16 @@
           </div>
         </div>
       </header>
-  
+
       <!-- Main Content -->
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
-            {!! \App\Section::query()->where('name', 'aboutme')->get()[0]->description !!}
+            @if(\App\Section::query()->where('name', 'aboutme')->count() > 0)
+              {!! \App\Section::query()->where('name', 'aboutme')->get()[0]->description !!}
+            @endif
           </div>
         </div>
       </div>
-  
+
 @endsection
